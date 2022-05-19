a6abdb5029152b7e6807bf494bc8264da0d04daa
dieres <dieres2010@gmail.com>
2022-05-18 20:43:55 -0400

    bio updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
       <img src={coverImage} className="my-2" style={{ width: "15%", height: "15%"}} class="img-fluid" alt="cover" />
       <div className="my-2">
         <p>
-          My name is Diego Restrepo and I am a Software Engineering and a recently graduated Full Stack Web Developer from UCF coding bootcamp. I am a fast learner and accomplished coder and programmer, and I will enjoy using my skills to contribute to my future employers. I graduated in 1998 from the University Antonio Narino in Medellin, Colombia with a Bachelor's Degree in Software Development. I love cats and like to do home remodeling
+          My name is Diego Restrepo and I am a Software Engineering and a recently graduated Full Stack Web Developer from UCF coding bootcamp. I am a fast learner and accomplished coder and programmer, and I will enjoy using my skills to contribute to my future employers. I graduated in 1998 from the University Antonio Narino in Medellin, Colombia with a Bachelor's Degree in Software Engineering. I love cats and like to do home remodeling
       </p>
       </div>
     </section>
